3cf14549c93c7563fdb3d0da57f3bb8df116a0e6
bsdl <sdlrr24@outlook.com>
2020-02-01 23:07:51 +0800

    update

--- src/com/bsdl/hard/_0239/Sliding_Window_Maximum.java
+++ src/com/bsdl/hard/_0239/Sliding_Window_Maximum.java
@@ -1,11 +1,89 @@
 package com.bsdl.hard._0239;
 
+
 import java.util.*;
 
 class Sliding_Window_Maximum {
+    // O(n * k)
+//    public int[] maxSlidingWindow(int[] nums, int k) {
+//        if (nums.length == 0) {
+//            return new int[]{};
+//        }
+//        int[] maxes = new int[nums.length - k + 1];
+//        for (int i = 0; i <= nums.length - k; i++) {
+//            int max = Integer.MIN_VALUE;
+//            for (int j = i; j < i + k; j++) {
+//                max = Math.max(max, nums[j]);
+//            }
+//            maxes[i] = max;
+//        }
+//        return maxes;
+//    }
+
+//    public int[] maxSlidingWindow(int[] nums, int k) {
+//        if (nums.length == 0) {
+//            return new int[]{};
+//        }
+//        int[] maxes = new int[nums.length - k + 1];
+//        Deque<Integer> deque = new LinkedList<>();
+//        int i = 0;
+//        while (i < k) {
+//            if(!deque.isEmpty()) {
+//                System.out.println(nums[deque.peekFirst()]);
+//            }
+//            while (!deque.isEmpty() && nums[deque.peekFirst()] < nums[i]) {
+//                deque.pollFirst();
+//            }
+////            deque.offerFirst(nums[i++]);
+//            deque.offerFirst(i++);
+//        }
+//        int index = 0;
+//        maxes[index] = nums[deque.peekLast()];
+//        for (; i < nums.length; i++) {
+//
+//            while (!deque.isEmpty() && nums[deque.peekFirst()] <= nums[i]) {
+//                deque.pollFirst();
+//            }
+//            deque.offerFirst(i);
+//            if(!deque.isEmpty() && deque.peekLast() == i - k) {
+//                deque.pollLast();
+//            }
+//            maxes[index++] = nums[deque.peekLast()];
+//        }
+//        return maxes;
+//    }
+
     public int[] maxSlidingWindow(int[] nums, int k) {
-       int[] maxes = new int[nums.length - k + 1];
-       Deque<Integer> deque = new LinkedList<>();
-       return maxes;
+        if (nums.length == 0) {
+            return new int[]{};
+        }
+        int[] maxes = new int[nums.length - k + 1];
+        int windowRight = k - 1;
+        int lastMaxIndex = -1;
+        int windowLeft = 0;
+        for(; windowRight < nums.length; windowRight++) {
+            // 判断窗口是否已经删除了上一个最大的元素
+            // 如果已经删除则重新在窗口内比较找出最大值的索引
+            if(lastMaxIndex < windowLeft) {
+                int max = Integer.MIN_VALUE;
+                for(int i = windowLeft; i <= windowRight; i++) {
+                    if(max < nums[i]) {
+                        max = nums[i];
+                        lastMaxIndex = i;
+                    }
+                }
+            // 如果还在窗口内则只用和新进入的数进行比较
+            }else if(nums[lastMaxIndex] < nums[windowRight]) {
+                lastMaxIndex = windowRight;
+            }
+            maxes[windowLeft] = nums[lastMaxIndex];
+            windowLeft++;
+        }
+        return maxes;
+    }
+
+    public static void main(String[] args) {
+        Sliding_Window_Maximum s = new Sliding_Window_Maximum();
+        s.maxSlidingWindow(new int[]{7,2,4}, 2);
     }
 }
--- src/com/bsdl/medium/_0347/Top_K_Frequent_Elements.java
+++ src/com/bsdl/medium/_0347/Top_K_Frequent_Elements.java
@@ -2,8 +2,56 @@ package com.bsdl.medium._0347;
 
 import java.util.*;
 
+
+// 需要一个能够根据出现频率快速获取元素的数据结构,即优先队列
 class Top_K_Frequent_Elements {
-    // public List<Integer> topKFrequent(int[] nums, int k) {
-        
-    // }
+//    public List<Integer> topKFrequent(int[] nums, int k) {
+//        if (k == 0) {
+//            return new ArrayList<>();
+//        }
+//        List<Integer> list = new ArrayList<>();
+//        Map<Integer, Integer> map = new HashMap<>();
+//        for (int num : nums) {
+//             map.put(num, map.getOrDefault(num, 0) + 1);
+//        }
+//        PriorityQueue<Integer> pq = new PriorityQueue<>((a, b) -> map.get(a) - map.get(b));
+//        for(int key: map.keySet()) {
+//            if(pq.size() < k) {
+//                pq.add(key);
+//            }else if(map.get(key) > map.get(pq.peek())) {
+//                pq.remove();
+//                pq.add(key);
+//            }
+//        }
+//        while(!pq.isEmpty()) {
+//            list.add(pq.remove());
+//        }
+//        return list;
+//    }
+
+
+    public List<Integer> topKFrequent(int[] nums, int k) {
+        if (k == 0) {
+            return new ArrayList<>();
+        }
+        List<Integer> result = new ArrayList<>();
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int num : nums) {
+            map.put(num, map.getOrDefault(num, 0) + 1);
+        }
+        List<Integer>[] sortedOccur = new List[nums.length + 1];
+        for(int key: map.keySet()) {
+            int index = map.get(key);
+            if(sortedOccur[index] == null) {
+                sortedOccur[index] = new ArrayList<>();
+            }
+            sortedOccur[index].add(key);
+        }
+        for(int i = sortedOccur.length - 1; i >= 0; i--) {
+            if(sortedOccur[i] != null && result.size() < k) {
+                result.addAll(sortedOccur[i]);
+            }
+        }
+        return result;
+    }
 }
